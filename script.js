415196ab3e44d03e52ada59b2f4ce2c07e914b46
Andrii Balashov <92510548+andriiobalashov@users.noreply.github.com>
2022-05-23 16:09:56 +0200

    zeinachist

---
+++ script.js
@@ -0,0 +1,50 @@
+var rect = $('#hero-section')[0].getBoundingClientRect();
+var mouse = {x: 0, y: 0, moved: false};
+
+$("#hero-section").mousemove(function(e) {
+  mouse.moved = true;
+  mouse.x = e.clientX - rect.left;
+  mouse.y = e.clientY - rect.top;
+});
+ 
+TweenLite.ticker.addEventListener('tick', function(){
+  if (mouse.moved){    
+    parallaxIt(".mockup-bg", -100);
+    parallaxIt(".mockup", -30);
+    parallaxIt(".mockup-star-top", -10);
+    parallaxIt(".mockup-star-bottom", -10);
+  }
+  mouse.moved = false;
+});
+
+function parallaxIt(target, movement) {
+  TweenMax.to(target, 0.5, {
+    x: (mouse.x - rect.width / 2) / rect.width * movement,
+    y: (mouse.y - rect.height / 2) / rect.height * movement
+  });
+}
+
+$(window).on('resize scroll', function(){
+  rect = $('#hero-section')[0].getBoundingClientRect();
+})
+
+var texts = new Array();
+texts.push("entrepreneurs");
+texts.push("porteurs de projets");
+texts.push("influenceurs");
+texts.push("communiquants");
+texts.push("créateurs");
+
+var point = 0;
+
+function changeText(){
+  $('.change-word').html(texts[point]);
+  if(point < texts.length - 1){
+    point ++;
+  }else{
+    point = 0;
+  }
+  setTimeout(changeText, 5000)
+}
+
+changeText();
